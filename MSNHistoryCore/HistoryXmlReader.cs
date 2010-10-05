392d6229bd7a6790334353b5389587c91bf54345
Jeremy Cui <tsuijy@gmail.com>
2010-10-05 16:42:56 +0800

    initialize the solution and added some classes and tests

---
+++ MSNHistoryCore/HistoryXmlReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace MsnHistoryCore
+{
+    public class HistoryXmlReader
+    {
+        public static HistoryXmlReader Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        } private static HistoryXmlReader _instance = new HistoryXmlReader();
+
+        public MsnLog Read(string msnHistoryFile)
+        {
+            if (string.IsNullOrEmpty(msnHistoryFile)) throw new ArgumentNullException("The file name can not be null");
+            if (File.Exists(msnHistoryFile) == false) throw new FileNotFoundException("Can not find file" + msnHistoryFile);
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(msnHistoryFile);
+
+            var log = new MsnLog();
+            var logNode = xmlDoc.SelectSingleNode("Log");
+            //log = logNode.Attributes[log.FirstSessionID]
+
+            return null;
+        }
+
+    }
+}
---
+++ MSNHistoryCore/MsnLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace MsnHistoryCore
+{
+    public class MsnLog
+    {
+        public int FirstSessionID { get; set; }
+
+        public int LastSessionID { get; set; }
+
+    }
+}
